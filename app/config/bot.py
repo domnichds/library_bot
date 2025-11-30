e388326050de9b81b2be0cdecfaccc991c01e556
Данил Домнич <domni4.d2@yandex.ru>
2025-11-30 15:19:59 +0300

    Перенести базу данных на PostgreSQL

--- app/config/bot.py
+++ app/config/bot.py
@@ -7,3 +7,7 @@ load_dotenv()
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 if not BOT_TOKEN:
     raise RuntimeError("BOT_TOKEN is not set in the environment variables")
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+if not DATABASE_URL:
+    raise RuntimeError("DATABASE_URL is not set in the environment variables")
